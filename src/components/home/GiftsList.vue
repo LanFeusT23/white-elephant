f81693827f5feb0e1bdc33b341ba8849717ad423
robo <1719221+robodude@users.noreply.github.com>
2020-12-15 13:25:31 -0800

    fixed issue with gift display

--- src/components/home/GiftsList.vue
+++ src/components/home/GiftsList.vue
@@ -1,43 +1,33 @@
 <template>
     <div v-if="finalGifts.length >= 0" class="flex-1 gifts-list">
-        <Gift v-for="(gift, index) in finalGifts"
-            :key="gift.id"
-            v-bind="gift"
-            @click="openModal(index)">
-        </Gift>
+        <Gift v-for="(gift, index) in finalGifts" :key="gift.id" v-bind="gift" @click="openModal(index)"> </Gift>
     </div>
 
-    <div v-else class="flex items-center justify-center flex-1 text-4xl text-yellow-300" >
-        No gifts added yet!
-    </div>
+    <div v-else class="flex items-center justify-center flex-1 text-4xl text-yellow-300">No gifts added yet!</div>
 
     <!-- {{ finalGifts.map(x=>x.selectedByName) }} -->
 
     <teleport to="#modal-portal-target" v-if="isOpenModal">
-        <GiftsModal 
-            @close-modal="closeModal"
-            :gifts="finalGifts"
-            :selectedGiftIndex="selectedGiftIndex">
-        </GiftsModal>
+        <GiftsModal @close-modal="closeModal" :gifts="finalGifts" :selectedGiftIndex="selectedGiftIndex"> </GiftsModal>
     </teleport>
 </template>
 
 <script>
-import { computed, onUnmounted, ref, toRefs } from 'vue'
+import { computed, onUnmounted, ref, toRefs } from "vue"
 import Gift from "@/components/home/Gift"
 import GiftsModal from "@/components/home/GiftsModal"
 import Button from "@/components/shared/Button"
 import firebaseListChangeHelper from "@/helpers/firebaseListChangeHelper"
-import { firestore } from '@/firebase'
-import { useRoute } from 'vue-router'
-import { useStore } from 'vuex'
+import { firestore } from "@/firebase"
+import { useRoute } from "vue-router"
+import { useStore } from "vuex"
 
 export default {
-    setup () {
+    setup() {
         const route = useRoute()
         const store = useStore()
         const isOpenModal = ref(false)
-        const selectedGiftIndex = ref();
+        const selectedGiftIndex = ref()
 
         const openModal = (giftIndex) => {
             selectedGiftIndex.value = giftIndex
@@ -49,51 +39,55 @@ export default {
             isOpenModal.value = false
         }
 
-        const eventRef = firestore.collection("events").doc(route.params.eventId) 
+        const eventRef = firestore.collection("events").doc(route.params.eventId)
         const giftsRef = eventRef.collection("gifts")
         const usersRef = eventRef.collection("users")
 
-        
         const giftsList = ref([])
-        const unsubscribeGifts = giftsRef
-            .onSnapshot(snapshot => {
-                snapshot.docChanges().forEach((change) => {
-                    firebaseListChangeHelper(change, giftsList)
-                })
+        const unsubscribeGifts = giftsRef.where("selectedBy", "!=", null).onSnapshot((snapshot) => {
+            snapshot.docChanges().forEach((change) => {
+                firebaseListChangeHelper(change, giftsList)
+            })
         })
 
         const userList = ref([])
-        const unsubscribeUsers = usersRef//.where("selectedBy", "!=", null)
-            .onSnapshot(snapshot => {
-                snapshot.docChanges().forEach((change) => {
-                    firebaseListChangeHelper(change, userList)
-                })
+        const unsubscribeUsers = usersRef.onSnapshot((snapshot) => {
+            snapshot.docChanges().forEach((change) => {
+                firebaseListChangeHelper(change, userList)
+            })
         })
 
         const event = computed(() => {
             return store.state.event
         })
-        const finalGifts = computed(() => {
-            return giftsList.value.map(gift => {
-                // users and gifts share the same id from the user's uid
-                const userGift = userList.value.find(x => x.id === gift.id)
-                const userSelectedBy = userList.value.find(x => x.id === gift.selectedBy)
 
-                if (userGift) {
-                    console.log(userSelectedBy?.displayName);
+        const finalGifts = computed(() => {
+            const giftCards = userList.value.map((gift) => {
+                // users and gifts share the same id from the user's uid
+                const unwrappedGift = giftsList.value.find((x) => x.selectedBy === gift.id)
+                const selectedByUser = userList.value.find((x) => x.id === unwrappedGift?.selectedBy)
+
+                gift = {
+                    ...gift,
+                    selectedByName: "Not Selected",
+                    notAvailable: false,
+                }
+
+                if (unwrappedGift) {
                     gift = {
                         ...gift,
-                        ...userGift,
-                        selectedByName: userSelectedBy?.displayName,
-                        notAvailable: event.maxSteals <= gift.stolenCount
+                        ...unwrappedGift,
+                        selectedByName: selectedByUser?.displayName,
+                        notAvailable: event.maxSteals <= gift.stolenCount,
                     }
-
                 }
 
                 return gift
-            });
+            })
+
+            return giftCards
         })
-        
+
         onUnmounted(() => {
             unsubscribeGifts()
             unsubscribeUsers()
@@ -107,9 +101,9 @@ export default {
             openModal,
             closeModal,
             finalGifts,
-            selectedGiftIndex
+            selectedGiftIndex,
         }
-    }
+    },
 }
 </script>
 
