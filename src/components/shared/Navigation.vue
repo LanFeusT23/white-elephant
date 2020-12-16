ac24de5fa91379da9d6a1a0dd962eead23d44f27
robo <1719221+robodude@users.noreply.github.com>
2020-12-15 20:24:44 -0800

    cosmetic changes

--- src/components/shared/Navigation.vue
+++ src/components/shared/Navigation.vue
@@ -1,7 +1,9 @@
 <template>
     <div class="flex items-center justify-between h-24 gap-8 px-8 shadow-xl bg-opacity-90 bg-red-1000">
-        <img src="../../assets/images/logo.png" alt="Logo">
-        
+        <router-link to="/">
+            <img class="h-20" src="@/assets/images/white-elephant.png" alt="Logo" />
+        </router-link>
+
         <div id="navigation-portal-target" class="flex-1 h-full"></div>
 
         <div class="flex items-center gap-4">
@@ -12,28 +14,27 @@
                 <span class="text-sm cursor-pointer" v-if="user" @click="signOut">Log Out</span>
             </div>
         </div>
-
     </div>
 </template>
 
 <script>
-import { computed } from 'vue';
+import { computed } from "vue"
 import { useStore } from "vuex"
 import { auth } from "@/firebase"
 
 export default {
-    setup () {
+    setup() {
         const store = useStore()
         const signOut = () => {
             auth.signOut()
         }
 
-        const user = computed(() => store.state.user);
+        const user = computed(() => store.state.user)
 
         return {
             user,
-            signOut
+            signOut,
         }
-    }
+    },
 }
 </script>
--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -1,9 +1,19 @@
 <template>
-    <div>
-        LANDING
+    <div class="container flex flex-col items-center align-middle justify-items-center">
+        <h1 class="text-5xl text-yellow-300">Welcome to (NAME OF THE WEBSITE)!!</h1>
 
-        <router-link :to="EVENTSLANDING">See Events</router-link>
+        <img class="object-scale-down h-96" src="@/assets/images/white-elephant.png" alt="Logo" />
+
+        <router-link class="text-3xl hover:text-yellow-300" :to="EVENTSLANDING">See Upcoming Events</router-link>
     </div>
+
+    <teleport to="#navigation-portal-target">
+        <div class="flex items-center justify-between w-full h-full">
+            <div class="flex items-center gap-4">
+                <span class="text-5xl"> NAME OF THE WEBSITE </span>
+            </div>
+        </div>
+    </teleport>
 </template>
 
 <script>
