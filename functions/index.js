35c7bff93ab0bc7d903ec65a043a4790719007a6
robo <1719221+robodude@users.noreply.github.com>
2020-12-15 19:59:07 -0800

    after everyone has a gift picked out we roll back to player #1

--- functions/index.js
+++ functions/index.js
@@ -6,135 +6,6 @@ const _shuffle = require("lodash/shuffle")
 admin.initializeApp()
 const firestore = admin.firestore()
 
-exports.bootstrap = functions.https.onRequest(async (request, response) => {
-    let eventRef = firestore.collection("events").doc("capps-2020")
-
-    eventRef.set({
-        name: "CAPPS WHITE ELEPHANT 2020",
-        createdBy: "user-a",
-        currentPlayer: null,
-        maxSteals: 3,
-        gameStarted: false
-    })
-
-    ////// Adding Users
-
-    eventRef
-        .collection("users")
-        .doc("user-a")
-        .set({
-            imageUrl: "user-a.png",
-            order: null,
-            readyToPlay: false,
-            selectedGift: false
-        })
-
-    eventRef
-        .collection("gifts")
-        .doc("user-a")
-        .set({
-            revealed: false,
-            imageUrl: "user-a.png",
-            description: "user-a toy"
-        })
-
-    eventRef
-        .collection("users")
-        .doc("user-b")
-        .set({
-            imageUrl: "user-b.png",
-            order: null,
-            readyToPlay: false,
-            selectedGift: false
-        })
-
-    eventRef
-        .collection("gifts")
-        .doc("user-b")
-        .set({
-            revealed: false,
-            imageUrl: "user-b.png",
-            description: "user-b toy"
-        })
-
-    eventRef
-        .collection("users")
-        .doc("user-c")
-        .set({
-            imageUrl: "user-c.png",
-            order: null,
-            readyToPlay: false,
-            selectedGift: false
-        })
-
-    eventRef
-        .collection("gifts")
-        .doc("user-c")
-        .set({
-            revealed: false,
-            imageUrl: "user-c.png",
-            description: "user-c toy"
-        })
-
-    eventRef
-        .collection("users")
-        .doc("user-d")
-        .set({
-            imageUrl: "user-d.png",
-            order: null,
-            readyToPlay: false,
-            selectedGift: false
-        })
-
-    eventRef
-        .collection("gifts")
-        .doc("user-d")
-        .set({
-            revealed: false,
-            imageUrl: "user-d.png",
-            description: "user-d toy"
-        })
-
-    /// Everyone should be ready to go
-    await new Promise(resolve => setTimeout(resolve, 3000))
-
-    await eventRef.update({
-        started: true
-    })
-
-    //game is now initialized, players have an order
-    await new Promise(resolve => setTimeout(resolve, 3000))
-
-    // let eventData = (await eventRef.get()).data()
-    // let currentPlayer = eventData.currentPlayer
-
-    // let giftsQuerySnapshop = await eventRef
-    //     .collection("gifts")
-    //     .where("revealed", "==", false)
-    //     .limit(1)
-    //     .get()
-
-    // let docRef = giftsQuerySnapshop.docs[0]
-
-    // eventRef
-    //     .collection("gifts")
-    //     .doc(docRef.id)
-    //     .update({
-    //         selectedBy: currentPlayer,
-    //         revealed: true
-    //     })
-
-    // eventRef
-    //     .collection("user")
-    //     .doc(docRef.id)
-    //     .update({
-    //         selectedGift: true
-    //     })
-
-    functions.logger.info("Hello logs!", { structuredData: true })
-    response.send("BOOTSTRAPPING")
-})
-
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
@@ -214,23 +85,38 @@ exports.giftUpdate = functions.firestore.document("events/{eventId}/gifts/{userI
             })
         } else {
             //gets the next player
-            const snap = await eventRef
+            const nextPlayerSnap = await eventRef
                 .collection("users")
                 .where("selectedGift", "==", false)
                 .orderBy("order")
                 .limit(1)
                 .get()
 
-            if (!snap.empty) {
-                const user = snap.docs[0]
+            if (!nextPlayerSnap.empty) {
+                const nextPlayer = nextPlayerSnap.docs[0]
 
                 //set the next player on the event
                 eventRef.update({
-                    currentPlayer: user.id
+                    currentPlayer: nextPlayer.id
                 })
             } else {
                 //ENABLE BONUS ROUND OF DEATH
                 console.log("NO MORE")
+
+                const firstPlayerSnap = await eventRef
+                    .collection("users")
+                    .orderBy("order")
+                    .limit(1)
+                    .get()
+
+                if (!firstPlayerSnap.empty) {
+                    const firstPlayer = firstPlayerSnap.docs[0]
+
+                    //set to the first player for the final round.
+                    eventRef.update({
+                        currentPlayer: firstPlayer.id
+                    })
+                }
             }
         }
     }
