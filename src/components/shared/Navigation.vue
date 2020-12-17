e2305e7d0dcd4cfd93626039d328f41b5caac1df
robo <1719221+robodude@users.noreply.github.com>
2020-12-16 20:27:37 -0800

    animate elephant

--- src/components/shared/Navigation.vue
+++ src/components/shared/Navigation.vue
@@ -1,6 +1,8 @@
 <template>
     <div class="flex items-center justify-between h-24 gap-8 px-8 shadow-xl bg-opacity-90 bg-red-1000">
-        <router-link to="/">
+        <div ref="snowflake" class="absolute opacity-0">❄️</div>
+
+        <router-link @mouseenter="hovered = true" @mouseleave="hovered = false" to="/">
             <img class="h-20" src="@/assets/images/white-elephant.png" alt="Logo" />
         </router-link>
 
@@ -18,9 +20,14 @@
 </template>
 
 <script>
-import { computed } from "vue"
+import { computed, ref, watch, onMounted } from "vue"
 import { useStore } from "vuex"
 import { auth } from "@/firebase"
+
+import { gsap } from "gsap"
+import { Physics2DPlugin } from "gsap/Physics2DPlugin"
+
+gsap.registerPlugin(Physics2DPlugin)
 
 export default {
     setup() {
@@ -31,9 +38,54 @@ export default {
 
         const user = computed(() => store.state.user)
 
+        const hovered = ref(false)
+        const snowflake = ref(null)
+
+        const tl = gsap.timeline()
+        tl.pause()
+
+        onMounted(() => {
+            tl.set(snowflake.value, {
+                x: -6,
+            })
+
+            tl.to(snowflake.value, {
+                opacity: 1,
+                duration: 0.25,
+            })
+
+            tl.to(
+                snowflake.value,
+                {
+                    duration: 5,
+                    physics2D: {
+                        velocity: 100,
+                        angle: -101,
+                        gravity: 500,
+                    },
+                },
+                "-=.25"
+            )
+
+            tl.to(snowflake.value, {
+                duration: 5,
+                opacity: 0,
+            })
+        })
+
+        watch(hovered, (h) => {
+            if (h) {
+                tl.restart()
+            } else {
+                tl.restart().pause()
+            }
+        })
+
         return {
             user,
             signOut,
+            hovered,
+            snowflake,
         }
     },
 }
