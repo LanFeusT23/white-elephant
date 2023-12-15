1c0787d93172fbd2efe80bb2a1199d6bd1a5e259
LanFeusT23 <lanfeust23@gmail.com>
2023-12-15 10:49:28 -0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    purge: { content: ["./public/**/*.html", "./src/**/*.vue"] },
-    darkMode: false, // or 'media' or 'class'
+   content: ["./public/**/*.html", "./src/**/*.vue"],
+    darkMode: "class",
     theme: {
         extend: {
             fontFamily: {
